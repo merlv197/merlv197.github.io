c3624feef2f8bfb433bcbd4a1f26f609a0515ae0
merlv197 <merlv197@newschool.edu>
2017-07-24 11:21:32 +0200

    coding projects

---
+++ perso/graphic/app.js
@@ -0,0 +1,15 @@
+$(".holder4_4").click(function(){
+		
+
+    $(".holder4_5").fadeOut();
+    $(".holder4_5").fadeIn();
+   	$(".holder4_5").fadeOut();
+   	$(".holder4_5").fadeIn();
+   	$(".holder4_5").fadeOut();
+   	$(".holder4_5").fadeIn();
+   	$(".holder4_5").fadeOut();
+   	$(".holder4_5").fadeIn();
+   	$(".holder4_5").fadeOut();
+
+});  	
+   	
